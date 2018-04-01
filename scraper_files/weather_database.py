4b3431fb59ee360031e846e7b68ecbe838e221fc
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-04-01 15:38:37 +0100

    Adding changes to file that extracts json data properly and populates weather_info table with all of the data requested from JSON

--- scraper_files/weather_database.py
+++ scraper_files/weather_database.py
@@ -21,26 +21,28 @@ metadata = MetaData()
 
 class json_data:
     def data(self):
-        for i in json_weather:
-            message = i['message']
-            list = i['list']
-            main = list['main']
+        list = json_weather['list']
+        i = 0
+        length = len(list)
+        while i < length:
+            access = list[i]
+            main = access['main']
             temp = main['temp']
             temp_min = main['temp_min']
             temp_max = main['temp_max']
-            weather = list['weather']
-            maindescrip = weather['main']
-            description = weather['description']
-            wind = list['wind']
+            weather = access['weather']
+            weathered = weather[0]
+            description = weathered['description']
+            #maindescription = weathered['main']
+            wind = access['wind']
             speed = wind['speed']
             deg = wind['deg']
-            dt_txt = list['dt_txt']
-
-        insert_weather(temp, temp_min, temp_max, speed, maindescrip, description, deg, dt_txt)
+            #dt_txt = list['dt_txt']
+            print(temp)
+            i += 1
+            insert_weather(temp, temp_min, temp_max, description, speed, deg)
 
     # http://pythondata.com/collecting-storing-tweets-python-mysql/
-
-
 
 def connect():
     """Function to connect to database on Amazon Web Services"""
@@ -67,29 +69,31 @@ def create_table(databasename):
                         Column('temp', Float(40)),
                         Column('temp_min', Float(40)),
                         Column('temp_max', Float(40)),
-                        Column('maindescrip', String(40)),
+                       # Column('maindescription', String(40)),
                         Column('description', String(40)),
                         Column('speed', Float(40)),
-                        Column('deg', Float(40)),
-                        Column('dt_txt', String(40)))
+                        Column('deg', Float(40)))
+                        #Column('dt_txt', String(40)))
     metadata.create_all(engine, checkfirst=True)
 
     # http://docs.sqlalchemy.org/en/latest/core/metadata.html
 
 
-def insert_weather(temp, temp_min, temp_max, speed, maindescrip, description, deg, dt_txt):
+def insert_weather(temp, temp_min, temp_max, speed, description, deg):
     connection = engine.connect()
     connection.execute(
-        "INSERT INTO weather_info (temp, temp_min, temp_max, speed, maindescrip, description, deg, dt_txt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
-        (temp, temp_min, temp_max, speed, maindescrip, description, deg, dt_txt))
+        "INSERT INTO weather_info (temp, temp_min, temp_max, description, speed, deg) VALUES (%s, %s, %s, %s, %s, %s);",
+        (temp, temp_min, temp_max, description, speed, deg))
     return
 
 
 
-url = "http://api.openweathermap.org/data/2.5/forecast?id=5344157&APPID=c8610607135716bfdae60b788f8e0c8d"
+url = "http://api.openweathermap.org/data/2.5/forecast?id=2964574&APPID=c8610607135716bfdae60b788f8e0c8d"
 engine = connect()
-create_table(engine)
+#create_table(engine)
 weatherData = ca.call_weather_api(url)
 json_weather = ca.write_weather(weatherData)
+run=json_data()
+run.data()
 # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html
 
