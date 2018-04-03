0adcd3acbd01f3591ceea86b4b16170a6cd82ed6
Aoife OSullivan <aoifeosullivan19@gmail.com>
2018-04-03 23:45:02 +0100

    changed my code to match Amy's reworked code

--- FlaskApp/app/dbcon.py
+++ FlaskApp/app/dbcon.py
@@ -1,28 +1,33 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, Float, Unicode
+from sqlalchemy.ext.declarative import declarative_base
 import pymysql
 
-app = Flask(__name__)
+Base = declarative_base()
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://dublinbikesadmin:dublinbikes2018@dublinbikes.cglcinwmtg3w.eu-west-1.rds.amazonaws.com/dublinbikes'
-db = SQLAlchemy(app)
-
-class Examp(db.Model):
+class Examp(Base):
 	__tablename__ = 'static_info'
-	id = db.Column('number', db.Integer, primary_key=True)
-	location = db.Column('name', db.Unicode)
-	lat = db.Column('latitude', db.Float)
-	long = db.Column('longitude', db.Float)
-	stand = db.Column('bike_stands', db.Integer)
+	id = Column('number', Integer, primary_key=True)
+	location =Column('address', Unicode)
+	lat = Column('latitude', Float)
+	long = Column('longitude', Float)
+	stand = Column('bike_stands', Integer)
 
-class Dynamic(db.Model):
+class Dynamic(Base):
+	__tablename__ = 'dynamic_info'
+	id = Column('number', Integer, primary_key=True)
+	bikes = Column('available_bikes', Integer)
+	stands = Column('available_bike_stands', Integer)
+#	time = Column('last_update', db.DateTime)
+
+
+class latest(Base):
         __tablename__ = 'latest_info'
-        id = db.Column('number', db.Integer, primary_key=True)
-        bikes = db.Column('available_bikes', db.Integer)
-        stands = db.Column('available_bike_stands', db.Integer)
+        id = Column('number', Integer, primary_key=True)
+        bikes = Column('available_bikes', Integer)
+        stands = Column('available_bike_stands', Integer)
+#       time = Column('last_update', db.DateTime)
 
 
-test = Examp.query.all()
-test_dynam = Dynamic.query.all()
 #https://www.youtube.com/watch?v=Tu4vRU4lt6k
-
--- FlaskApp/app/view.py
+++ FlaskApp/app/view.py
@@ -1,16 +1,47 @@
 """Main module."""
-from flask import render_template
+from flask import render_template, jsonify, request
 from app import app
-from app.dbcon import Examp, Dynamic
+from app.dbcon import Examp, Dynamic, latest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import pygal
 
-#app = Flask(__name__)
+engine = create_engine('mysql+pymysql://dublinbikesadmin:dublinbikes2018@dublinbikes.cglcinwmtg3w.eu-west-1.rds.amazonaws.com/dublinbikes')
 
-@app.route('/', methods=['GET', 'POST'])
+Session = sessionmaker(bind=engine, autocommit=True)
+session = Session()
+
+@app.route('/')
 def index():
-	rows = Examp.query.all()
-	dynamic = Dynamic.query.all()
-	return render_template("index.html", rows=rows, dynamic = dynamic)
 
+	return render_template("index.html")
+
+@app.route('/station_info')
+def station_info():
+	try:
+		info = session.query(Examp.location, Examp.lat, Examp.long, Examp.id, Examp.stand)
+		arr = []
+		for i in info:
+			arr.append(i)
+		return jsonify(result = arr)
+	except Exception as e:
+		return str(e)
+
+@app.route('/station_occupancy')
+def station_occupancy():
+	try:
+		id = request.args.get('id')
+		sql = session.query(latest.bikes, latest.stands).filter(latest.id == id)
+		arr = []
+		print(sql)
+		for i in sql:
+			arr.append(i[0])
+			arr.append(i[1])
+		print(arr)
+		return jsonify(occupancy = arr)
+	except Exception as e:
+		return str(e)
 
 if __name__ == '__main__':
 	app.run(debug=True)
+
