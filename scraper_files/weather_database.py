9788a2e6a9face91f4ed0cb8b088eceff098fd57
Muireann Mac Carthy <muireann.maccarthy@ucdconnect.ie>
2018-04-04 22:26:38 +0100

    Added in rain data and main description into tables. Added API call into this script. tidied up code and added error handling.

--- scraper_files/weather_database.py
+++ scraper_files/weather_database.py
@@ -1,6 +1,5 @@
 import requests
 import json
-import pandas as pd
 import mysql.connector
 from mysql.connector import errorcode
 import sqlalchemy
@@ -9,7 +8,20 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import *
-import call_weather_api as ca
+
+def call_weather_api(url):
+    """Calls weather API and returns info"""
+
+    req = requests.get(url)
+    return req
+
+def write_weather (weatherData):
+    """Retrieves weather information from openweathermap and stores as JSON"""
+
+    req_weather = weatherData.text
+    json_weather = json.loads(req_weather)
+    print(json_weather)
+    return json_weather
 
 # http://docs.sqlalchemy.org/en/latest/orm/tutorial.html
 Base = declarative_base()
@@ -17,12 +29,9 @@ Session = sessionmaker()
 metadata = MetaData()
 
 
-class json_data:
-    def data(self):
+class json_weather_data:
+    def weather_data(self):
         list = json_weather['list']
-        #i = 0
-        length = len(list)
-        #while i < length:
         access = list[0]
         main = access['main']
         temp = main['temp']
@@ -32,15 +41,23 @@ class json_data:
         weather = access['weather']
         weathered = weather[0]
         description = weathered['description']
+        mainDescription = weathered['main']
         wind = access['wind']
         speed = wind['speed']
         deg = wind['deg']
         dt_txt = access['dt_txt']
-            #print(temp)
-            #i += 1
-        insert_weather(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity)
-        delete_rows(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity)
-        insert_dynamic_weather(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity)
+        if 'rain' in access:
+            rained = access['rain']
+            if '3h' in rained:
+                rain = rained['3h']
+            else:
+                rain = 0.0
+        else:
+            rain = 0.0
+        insert_weather(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain)
+        delete_rows(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain)
+        insert_dynamic_weather(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity,
+                               rain)
 
     # http://pythondata.com/collecting-storing-tweets-python-mysql/
 
@@ -48,7 +65,6 @@ class json_data:
 def connect():
     """Function to connect to database on Amazon Web Services"""
     try:
-
         engine = create_engine(
             'mysql+mysqlconnector://dublinbikesadmin:dublinbikes2018@dublinbikes.cglcinwmtg3w.eu-west-1.rds.amazonaws.com/dublinbikes')
         port = 3306
@@ -57,8 +73,8 @@ def connect():
         return engine
         # https://campus.datacamp.com/courses/introduction-to-relational-databases-in-python/advanced-sqlalchemy-queries?ex=2#skiponboarding
 
-    except Exception as err:
-        print("An error occurred when connecting to the database: ", err)
+    except Exception as e:
+        print("An error occurred when connecting to the database: ", e)
         # https://dev.mysql.com/doc/connector-python/en/connector-python-api-errors-error.html
     # https://campus.datacamp.com/courses/introduction-to-relational-databases-in-python/advanced-sqlalchemy-queries?ex=2#skiponboarding
 
@@ -73,10 +89,12 @@ def create_static_table(databasename):
                         Column('temp_min', Float(40)),
                         Column('temp_max', Float(40)),
                         Column('description', String(40)),
+                        Column('mainDescription', String(40)),
                         Column('speed', Float(40)),
                         Column('deg', Float(40)),
                         Column('dt_txt', String(60)),
-                        Column('humidity', Float(40)))
+                        Column('humidity', Float(40)),
+                        Column('rain', Float(40)))
 
     metadata.create_all(engine, checkfirst=True)
 
@@ -90,43 +108,54 @@ def create_dynamic_table(databasename):
                         Column('temp_min', Float(40)),
                         Column('temp_max', Float(40)),
                         Column('description', String(40)),
+                        Column('mainDescription', String(40)),
                         Column('speed', Float(40)),
                         Column('deg', Float(40)),
                         Column('dt_txt', String(60)),
-                        Column('humidity', Float(40)))
+                        Column('humidity', Float(40)),
+                        Column('rain', Float(40)))
 
     metadata.create_all(engine, checkfirst=True)
 
     # http://docs.sqlalchemy.org/en/latest/core/metadata.html
 
 
-def insert_weather(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity):
-    connection = engine.connect()
-    connection.execute(
-        "INSERT INTO weather_info (temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
-        (temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity))
+def insert_weather(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain):
+    try:
+        connection = engine.connect()
+        connection.execute(
+            "INSERT INTO weather_info (temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+            (temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain))
+    except Exception as e:
+        print("An error occurred inserting data into weather_info : ", e)
     return
 
-def delete_rows(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity):
+
+def delete_rows(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain):
+    try:
         connection = engine.connect()
         connection.execute("TRUNCATE TABLE dynamic_weather;")
         return
 
-def insert_dynamic_weather(temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity):
-    connection = engine.connect()
-    connection.execute ("INSERT INTO dynamic_weather (temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity) VALUES (%s, %s, %s, %s, %s, %s, %s, %s);",
-                        (temp, temp_min, temp_max, description, speed, deg, dt_txt, humidity))
+    except Exception as e:
+        print("An error occurred when deleting rows: ", e)
+
+def insert_dynamic_weather(temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain):
+    try:
+        connection = engine.connect()
+        connection.execute(
+            "INSERT INTO dynamic_weather (temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s);",
+            (temp, temp_min, temp_max, description, mainDescription, speed, deg, dt_txt, humidity, rain))
+    except Exception as e:
+        print("An error occurred inserting data into dynamic_weather: ", e)
     return
-
-
 
 url = "http://api.openweathermap.org/data/2.5/forecast?id=2964574&units=metric&APPID=c8610607135716bfdae60b788f8e0c8d"
 engine = connect()
 create_dynamic_table(engine)
 create_static_table(engine)
-weatherData = ca.call_weather_api(url)
-json_weather = ca.write_weather(weatherData)
-run=json_data()
-run.data()
+weatherData = call_weather_api(url)
+json_weather = write_weather(weatherData)
+run = json_weather_data()
+run.weather_data()
 # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.to_sql.html
-
